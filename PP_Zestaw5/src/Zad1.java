db84acbdcb6c8a8d375f56cee1889e88fe298c35
PAWEŁ <PAWE@192.168.1.71>
2017-12-10 18:51:30 +0100

    Gotowe

--- PP_Zestaw5/src/Zad1.java
+++ PP_Zestaw5/src/Zad1.java
@@ -1,9 +1,32 @@
-
+/* Napisz funkcjê, która zwraca wartoœæ silni dla podanej liczby n. Funkcja powinna
+ * byæ napisana w dwóch wersjach: iteracyjnej i rekurencyjnej.
+ */
+import java.util.Scanner;
 public class Zad1 {
 
+	public static int silniaIteracja(int i) {
+		int m;
+		int wynik = 1;
+		for(m=1; m<=i; m++) {
+		wynik *= m;
+		}
+			return wynik;	
+	}
+	public static int silniaRekurencja(int i) {
+		if (i<1) {
+			return 1;
+		} else {
+			return i*silniaRekurencja(i-1);
+			}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n;
+		Scanner s = new Scanner(System.in);
+		n = s.nextInt();
+		System.out.println("Podaj liczbê: ");
+		System.out.println(n + "! = " + silniaRekurencja(n));
+		System.out.println(n + "! = "+ silniaIteracja(n));
+		s.close();
 	}
 
 }
